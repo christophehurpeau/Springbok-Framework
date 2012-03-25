f09b6cd8e8c26f968952cfbce30f448c82c1b895
Christophe Hurpeau <christophe@hurpeau.com>
2012-03-25 23:48:38 +0200

    PaginationOverrun exception extends a 404 not found

--- springbok/FPaginationOverrunException.php
+++ springbok/FPaginationOverrunException.php
@@ -1,4 +1,6 @@
 <?php
-class FPaginationOverrunException extends Exception{
-	
+class FPaginationOverrunException extends HttpException{
+	public function __construct(){
+		parent::__construct(404,'Not Found',_tC('The page you requested was not found'));
+	}
 }
