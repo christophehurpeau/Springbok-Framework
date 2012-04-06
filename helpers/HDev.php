48b3a184f767c8be0747d5ff348630c249461175
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-04-06 10:43:56 +0200

    Springbok Bar for Mobile

--- helpers/HDev.php
+++ helpers/HDev.php
@@ -1,9 +1,10 @@
 <?php
 class HDev{
 	public static function springbokBar(){
+		if(CHttpRequest::isMobile()) return;
 		echo HHtml::cssInline(file_get_contents(CORE.'includes/springbokBar.css'));
 		echo HHtml::jsInline(file_get_contents(CORE.'includes/springbokBar.js'));
-		echo '<div id="springbok-bar"><b>Springbok</b>'
+		echo '<div id="springbok-bar"><b onclick="$(\'#springbok-bar\').fadeOut()">Springbok</b>'
 			.' | <a href="javascript:;" rel="changes">Changes ('.(empty(App::$changes) || empty(App::$changes[0][1]['all']) ?'0':count(App::$changes[0][1]['all'])).')</a>'
 			.' | <a href="javascript:;" rel="queries">Queries ('.(!class_exists('DB',false)?'0':(array_sum(array_map(function(&$db){return count($db->getQueries());},DB::getAll())))).')</a>'
 			.' | <a href="javascript:;" rel="route">Route</a>'
