e9efe6d86cadf61a37ae55d8bdf8e96487cc9272
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-05-04 19:03:03 +0200

    js ui

--- includes/js/ui/ulFilter.js
+++ includes/js/ui/ulFilter.js
@@ -1,5 +1,5 @@
 (function($){
-	$.fn.ulFilter=function(filter){
+	$.fn.sUlFilter=function(filter){
 		if(!filter) this.find('li').removeClass('hidden');
 		else{
 			this.find('li').each(function(i,li){
