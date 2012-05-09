705f750655240a751176c8087872da391c05a444
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-05-09 16:13:40 +0200

    Bug BASE_URL===''

--- _init.php
+++ _init.php
@@ -1,5 +1,5 @@
 <?php
 Springbok::$scriptname=substr(basename($_SERVER['SCRIPT_NAME']),0,-4);
-define('BASE_URL',substr($_SERVER['SCRIPT_NAME'], 0,-strlen(Springbok::$scriptname)-5));
+define('BASE_URL',($baseUrl=substr($_SERVER['SCRIPT_NAME'], 0,-strlen(Springbok::$scriptname)-5))===''?'/':$baseUrl);
 define('IS_HTTPS',/*isset($_SERVER['HTTPS']) ? */!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'/* : false (substr($_SERVER['SCRIPT_NAME'],0,8)=='https://'))*/);
 if(isset($_SERVER['HTTP_HOST'])) define('FULL_BASE_URL','http'.( IS_HTTPS ? 's':'').'://'.$_SERVER['HTTP_HOST']);
