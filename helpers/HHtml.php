5a5064e59ca4f2926db1f570dfa3d6d2c7b9dc1a
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-05-25 10:58:00 +0200

    HHtml::ganalytics

--- helpers/HHtml.php
+++ helpers/HHtml.php
@@ -53,7 +53,7 @@ class HHtml{
 		echo self::cssInline(ob_get_clean());
 	}
 
-	public static function jsInline($content,$attributes=array()){
+	public static function jsInline($content){
 		return '<script type="text/javascript">
 //<![CDATA[
 '.trim($content).'
@@ -110,14 +110,15 @@ class HHtml{
 	}
 	
 	
-	public static function ganalytics($code){
-		return '<script type="text/javascript">
-var _gaq=_gaq || [];_gaq.push(["_setAccount","'.$code.'"]); _gaq.push([\'_trackPageview\']);
-(function(){
-var ga=document.createElement(\'script\');ga.type=\'text/javascript\'; ga.async=true;
-ga.src=(\'https:\' == document.location.protocol?\'https://ssl\':\'http://www\')+\'.google-analytics.com/ga.js\';
-var s=document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga,s);
-})();
+	public static function ganalytics($code,$trackPageLoadTime=false,$https=false){
+		return '<script type="text/javascript">//<![CDATA[
+var _gaq=[["_setAccount","'.$code.'"],[\'_trackPageview\']'.($trackPageLoadTime?",['_trackPageLoadTime']":'').'];
+(function(d,t){
+var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+g.type=\'text/javascript\';g.async=1;g.src=\''.($https?'https://ssl':'http://www').'.google-analytics.com/ga.js\';
+s.parentNode.insertBefore(g,s);
+})(document,\'script\');
+//]]>
 </script>';
 	}
 	
