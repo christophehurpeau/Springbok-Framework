567a18a24ca6757071e7de58fe4a2d1b595369dd
Christophe Hurpeau <christophe@hurpeau.com>
2012-06-04 23:29:21 +0200

    Css

--- enhancers/files/CssFile.php
+++ enhancers/files/CssFile.php
@@ -461,7 +461,8 @@ class CssFile extends EnhancerFile{
 							if(substr($url,0,8)==='COREIMG/'){
 								$key=$url;
 							}else{
-								if((!empty($matches[2]) && trim($matches[2])==='transparent') || substr($url,0,7) !== '../img/' || substr($url,-4)==='.gif' || substr($url,0,7+8) ==='../img/fancybox' || substr($url,0,7+6) ==='../img/mobile'
+								if((!empty($matches[2]) && ($trimMatches2=trim($matches[2])) && ($trimMatches2==='transparent' || (strlen($trimMatches2)===7) && $trimMatches2[0]==='#'))
+										|| substr($url,0,7) !== '../img/' || substr($url,-4)==='.gif' || substr($url,0,7+8) ==='../img/fancybox' || substr($url,0,7+6) ==='../img/mobile'
 										|| substr($url,0,7+8) === '../img/filetree' || substr($url,0,7+6) === '../img/jquery'
 										|| $url==='../img/'.$spritename){
 									return 'background'.$matches[1].':'.(empty($matches[2])?' ':$matches[2].' ').'url(\''.$url.'\')'.(empty($matches[4])?'':$matches[4]);
