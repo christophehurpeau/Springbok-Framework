71915f1ecbac38545c48b295bf57ef957a2c0bee
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-06-15 18:42:45 +0200

    Metas

--- helpers/HMeta.php
+++ helpers/HMeta.php
@@ -1,7 +1,7 @@
 <?php
 /** http://www.google.com/support/webmasters/bin/answer.py?answer=79812 */
 class HMeta{
-	private static $metas,$canonical;
+	private static $metas,$canonical,$prev,$next,$altLangs;
 	
 	public static function keywords($keywords){
 		self::$metas['keywords']=$keywords;
@@ -20,7 +20,11 @@ class HMeta{
 		self::$metas['google']='notranslate';
 	}
 	
-	public static function canonical($url){ self::$canonical=&$url; }
+	public static function canonical($url){ self::$canonical=$url; }
+	public static function prev($url){ self::$prev=$url; }
+	public static function next($url){ self::$next=$url; }
+	
+	public static function altlangs($urls){ self::$altLangs=$urls; }
 	
 	public static function display(){
 		if(self::$metas===null)return'';
@@ -32,6 +36,17 @@ class HMeta{
 	
 	public static function displayCanonical(){
 		/* DEV */ if(self::$canonical===null && !Springbok::$inError) throw new Exception("canonical is not defined"); /* /DEV */
-		return self::$canonical===false?'':'<link rel="canonical" href="'.HHtml::url(self::$canonical).'"/>';
+		if(self::$canonical===false) return '';
+		$result='<link rel="canonical" href="'.HHtml::url(self::$canonical).'"/>';
+		if(self::$prev!==null) $result.='<link rel="prev" href="'.HHtml::url(self::$prev).'"/>';
+		if(self::$next!==null) $result.='<link rel="next" href="'.HHtml::url(self::$next).'"/>';
+		return $result;
+	}
+	
+	public static function displayAltLangs(){
+		if(empty(self::$altLangs)) return '';
+		$result='';
+		foreach(self::$altLangs as $lang=>$url) '<link rel="alternate" hreflang="'.$lang.'" href="'.HHtml::url($url).'"/>';
+		
 	}
 }
