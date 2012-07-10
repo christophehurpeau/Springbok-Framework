d860811c215822eb6176cb6b159548b9020232ce
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-07-10 15:19:35 +0200

    eltClick : handle a[href="#"]

--- includes/js/springbok.base.js
+++ includes/js/springbok.base.js
@@ -131,9 +131,10 @@ window.S={
 		$(selector).off('click').each(function(i,elt){
 			elt=$(elt);
 			var trTimeout,href=elt.attr('rel');
-			elt.on('click',function(){trTimeout=setTimeout(function(){S.redirect(href)},250);});
+			elt.on('click',function(){trTimeout=setTimeout(function(){S.redirect(href)},180);});
 			elt.find('a[href]:not([href="#"])').off('click').on('click',function(e){ clearTimeout(trTimeout); e.stopPropagation(); e.preventDefault();
-					var a=$(this),confirmMessage=a.data('confirm'); if(!confirmMessage || confirm(confirmMessage=='1' ? i18nc['Are you sure ?'] : confirmMessage)) S.redirect(a.attr('href')); })
+					var a=$(this),confirmMessage=a.data('confirm'); if(!confirmMessage || confirm(confirmMessage=='1' ? i18nc['Are you sure ?'] : confirmMessage)) S.redirect(a.attr('href')); });
+			elt.find('a[href="#"]').off('click').on('click',function(e){ clearTimeout(trTimeout); e.stopPropagation(); e.preventDefault(); });
 		});
 		
 	}
