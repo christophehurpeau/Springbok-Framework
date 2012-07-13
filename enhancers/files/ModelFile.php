0a96599e0180fe95debbabe6c2c8a1206e4fa5ff
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-07-13 12:48:03 +0200

    ModelFile : auto QListName if not exists in the content

--- enhancers/files/ModelFile.php
+++ enhancers/files/ModelFile.php
@@ -106,7 +106,8 @@ class ModelFile extends PhpFile{
 												.' return $parent->'.($annotations['Child'][0][0]==='SearchablesKeyword'?'findIdOrInsert(\'slug\')':'insert()').'; }';
 					$classBeforeContent.='public function insertIgnoreParent(){ $parent=new '.$annotations['Child'][0][0].'; $parent->_copyData($this->data); return $parent->insertIgnore(); }';
 					$classBeforeContent.='public function updateParent(){ $parent=new '.$annotations['Child'][0][0].'; $parent->_copyData($this->data); return call_user_func_array(array($parent,"update"),func_get_args()); }';
-					$classBeforeContent.='public static function QListName(){ return parent::QList()->setFields(array("id"))->withParent("name"); }';
+					if(strpos($content,'function QListName(')===false)
+						$classBeforeContent.='public static function QListName(){ return parent::QList()->setFields(array("id"))->withParent("name"); }';
 					if($idField==='p_id') $classBeforeContent.='public static function getParentId($childId){ return self::QValue()->field("p_id")->byId($childId); }';
 					
 				}
