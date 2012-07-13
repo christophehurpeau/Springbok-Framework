2efae41e304c73db35f8ac5027bf461b7829cbd0
Christophe Hurpeau <christophe@hurpeau.com>
2012-07-13 23:37:15 +0200

    HMeta : escape url

--- helpers/HMeta.php
+++ helpers/HMeta.php
@@ -44,16 +44,16 @@ class HMeta{
 	public static function displayCanonical(){
 		/* DEV */ if(self::$canonical===null && !Springbok::$inError) throw new Exception("canonical is not defined"); /* /DEV */
 		if(self::$canonical===false) return '';
-		$result='<link rel="canonical" href="'.HHtml::url(self::$canonical,self::$canonicalEntry,true).'"/>';
-		if(self::$prev!==null) $result.='<link rel="prev" href="'.HHtml::url(self::$prev).'"/>';
-		if(self::$next!==null) $result.='<link rel="next" href="'.HHtml::url(self::$next).'"/>';
+		$result='<link rel="canonical" href="'.HHtml::urlEscape(self::$canonical,self::$canonicalEntry,true).'"/>';
+		if(self::$prev!==null) $result.='<link rel="prev" href="'.HHtml::urlEscape(self::$prev).'"/>';
+		if(self::$next!==null) $result.='<link rel="next" href="'.HHtml::urlEscape(self::$next).'"/>';
 		return $result;
 	}
 	
 	public static function displayAltLangs(){
 		if(empty(self::$altLangs)) return '';
 		$result='';
-		foreach(self::$altLangs as $lang=>$url) '<link rel="alternate" hreflang="'.$lang.'" href="'.HHtml::url($url).'"/>';
+		foreach(self::$altLangs as $lang=>$url) '<link rel="alternate" hreflang="'.$lang.'" href="'.HHtml::urlEscape($url).'"/>';
 		
 	}
 }
