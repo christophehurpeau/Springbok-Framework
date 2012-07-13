457466a521d9faa0d8b62f9641a512c776d4e0bd
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-07-13 19:15:15 +0200

    forms : defaultInput change bug

--- includes/js/springbok.forms.js
+++ includes/js/springbok.forms.js
@@ -21,7 +21,8 @@
 				$this.val(function(i,v){if(!$this.is(':focus') && (v=='' || v==this.title)) return this.title; $this.removeClass('default');return v;})
 				.focusin(function(){if($this.hasClass('default') || $this.val()===this.title) $this.removeClass('default').val('');})
 				.focusout(function(e){if(!$this.hasClass('default') && $this.val()=='') $this.addClass('default').val($this.attr('title'));})
-				.change(function(e){if($this.hasClass('default')) $this.val()!='' ? $this.removeClass('default') : $this.val($this.attr('title'))});
+				.change(function(e){ if($this.hasClass('default')){ if($this.val()!='') $this.removeClass('default'); else $this.val($this.attr('title')); }
+											else if($this.val()==''){ $this.addClass('default').val($this.attr('title')); }});
 			});
 			return inputs;
 		}else if(methods[method]){
