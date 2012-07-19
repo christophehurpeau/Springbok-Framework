ed8a0da55401e5cfcf3b446d5228e9225051c6a2
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-07-19 21:33:13 +0200

    RSS

--- helpers/HRss.php
+++ helpers/HRss.php
@@ -27,17 +27,17 @@ class HRss{
 		$this->_file=fopen(($file[0]==='/'?'':APP.'web/files/').$file,'w');
 		fwrite($this->_file,'<?xml version="1.0" encoding="UTF-8"?>
 <rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:taxo="http://purl.org/rss/1.0/modules/taxonomy/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">'
-			.'<channel><title>'.($title=h($title)).'</title><link>'.($link=HHtml::urlEscape($link,null,true)).'</link><description>'.$description.'</description><language>'.$lang.'</language>'
+			.'<channel><title>'.($title=h($title)).'</title><link>'.($link=HHtml::urlEscape($link,'index',true)).'</link><description>'.$description.'</description><language>'.$lang.'</language>'
 						.'<copyright>'.h($copyright).'</copyright><pubDate>'.($pubDate=date('r')).'</pubDate><lastBuildDate>'.$pubDate.'</lastBuildDate>'
 						.'<image><title>'.$title.'</title><url>'.h($image['url']).'</url><link>'.$link.'</link>'
 							.'<description>'.h($image['description']).'</description></image>'
-				.'<atom:link href="'.HHtml::staticUrl('/'.$file,'files').'" rel="self" type="application/rss+xml"/>');
+				.'<atom:link href="'.HHtml::urlEscape('/','index',true).'web/files/'.$file.'" rel="self" type="application/rss+xml"/>');
 	}
 
 	public function add($title,$link,$description,$pubDate,$enclosure=null){
 		fwrite($this->_file,'<item>'
 			.'<title>'.h($title).'</title>'
-			.'<link>'.($link=h(HHtml::url($link,null,true))).'</link>'
+			.'<link>'.($link=h(HHtml::url($link,'index',true))).'</link>'
 			.'<description>'.h($description).'</description>'
 			//.'<enclosure url="http://s1.lemde.fr/image/2012/01/05/87x0/1626126_7_0813_l-espagne-est-un-pays-solvable-a-reaffirme_b7eff2a082c6d8ec4a9745c29a62dd2a.jpg" length="1642" type="image/jpeg" />
 			.'<pubDate>'.date('r',strtotime($pubDate)).'</pubDate>'
