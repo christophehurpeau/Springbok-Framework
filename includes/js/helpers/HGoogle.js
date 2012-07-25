6056d4d0b6e765ed7a5fcadff115c7c82fd224c7
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-07-25 15:44:28 +0200

    js HGoogle

---
+++ includes/js/helpers/HGoogle.js
@@ -0,0 +1,25 @@
+S.HGoogle={
+	staticMap:function(size,zoom,center_or_path,markers,mapType){
+		var center,path,url='';
+		if(S.isArray(center_or_path)){ center=false; path=center_or_path }
+		else{ center=center_or_path; path=false; }
+		
+		if(center) url+='&center='+center;
+		if(path) url+='&path='+path.join('|');
+		if(zoom) url+='&zoom='+zoom;
+		if(markers){
+			if(markers.multiple) delete markers.multiple;
+			else markers=[markers];
+			markers.sbEach(function(i,marker){
+				url+='&markers=';
+				if(S.isObject(marker)){
+					url+=marker.style.join('|')+'|';
+					marker=marker.locations;
+				}
+				url+=marker.join('|');
+			});
+		}
+		url+='&size='+size+(mapType?'&maptype='+mapType:'');
+		return 'http://maps.googleapis.com/maps/api/staticmap?sensor=false'+url;
+	}
+};
