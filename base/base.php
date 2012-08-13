9e6b25e1c274528005e35d23b3d319ea03c669fb
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-08-13 10:07:31 +0200

    debugCode UTF-8

--- base/base.php
+++ base/base.php
@@ -133,7 +133,7 @@ function debug($object,$flush=true){
 	prettyDebug(htmlentities(print_r($object,true),ENT_QUOTES,'UTF-8'),2,$flush);
 }
 function debugCode($code,$withBacktrace=true){
-	prettyDebug(htmlentities((string)$code,ENT_QUOTES,'UTF-8',true),$withBacktrace?2:false,true);
+	prettyDebug(htmlentities(UEncoding::convertToUtf8((string)$code),ENT_QUOTES,'UTF-8',true),$withBacktrace?2:false,true);
 }
 function debugVar(){
 	ob_start();
