6d797d015e31d25f27cff09191bcd9afc0a37f13
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-08-30 16:11:13 +0200

    UString::checkAllLowerOrAllUpperCase

--- utils/UString.php
+++ utils/UString.php
@@ -1,4 +1,17 @@
 <?php
 class UString{
-	
+	public static function checkAllLowerOrAllUpperCase($string){
+		if(!empty($string)){
+			if($string===strtolower($string)) $string=ucfirst($string);
+			elseif($string===strtoupper($string)) $string=ucfirst(strtolower($string));
+			// http://stackoverflow.com/questions/1649015/check-if-at-least-75-of-a-string-is-uppercase
+			/*else{
+				preg_replace('/\p{Lu}/', '', $str,-1,$countUppercaseLetters);
+				if($countUppercaseLetters / strlen($string)) $string=ucfirst(strtolower($string));
+			}
+			if(preg_match('/^[\p{Lu}\s]*$/', $subject))
+			*/
+		}
+		return $string;
+	}
 }
