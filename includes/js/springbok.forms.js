2f1b8be9064626efe43b9462d965f54d695e16f0
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-09-07 13:26:51 +0200

    $.fn.ajaxForm

--- includes/js/springbok.forms.js
+++ includes/js/springbok.forms.js
@@ -43,44 +43,46 @@ includeCore('libs/jquery-ui-1.8.23.position');
 	};
 	
 	$.fn.ajaxForm=function(url,success,beforeSubmit,error){
-		//if(!error) error=function(jqXHR, textStatus){alert('Error: '+textStatus);};
-		var form=this,submit,imgLoadingSubmit=S.imgLoading();
-		if(S.isFunc(url)){
-			error=beforeSubmit;
-			beforeSubmit=success;
-			success=url;
-			url=undefined;
-		}
-		if(!url) url=form.attr('action');
-		this.unbind('submit').submit(function(evt){
-			evt.preventDefault();
-			evt.stopPropagation();
-			submit=form.find(':submit');
-			form.fadeTo(180,0.4);
-			if(window.tinyMCE!==undefined) tinyMCE.triggerSave();
-			if((beforeSubmit && beforeSubmit()===false) || (form.data('ht5ifv')!==undefined && !form.ht5ifv('valid'))){
-				form.stop().fadeTo(0,1)
-				return false;
+		this.each(function(){
+			//if(!error) error=function(jqXHR, textStatus){alert('Error: '+textStatus);};
+			var form=$(this),submit,imgLoadingSubmit=S.imgLoading();
+			if(S.isFunc(url)){
+				error=beforeSubmit;
+				beforeSubmit=success;
+				success=url;
+				url=undefined;
 			}
-			var ajaxOptions={
-				type:'post',cache:false,
-				beforeSend:function(){submit.hide();submit.parent().append(imgLoadingSubmit);},
-				data:form.serialize(),
-				complete:function(){submit.show().blur();form.find('.img.imgLoading').remove();form.fadeTo(150,1)},
-				error:error
-			};
-			if(success) ajaxOptions.success=success;
-			$.ajax(url,ajaxOptions)
-				.success(function(){S.bodyIcon('tick',form);})
-				.error(function(){S.bodyIcon('tick',cross);});
-			return false;
-		})
-		/*.find(':submit').unbind('click').click(function(){
-			//var validator=form.data('validator');
-			//if(validator && !validator.checkValidity()) return false;
-			//submit=$(this);
-			return true;
-		});*/
+			if(!url) url=form.attr('action');
+			form.unbind('submit').submit(function(evt){
+				evt.preventDefault();
+				evt.stopPropagation();
+				submit=form.find(':submit');
+				form.fadeTo(180,0.4);
+				if(window.tinyMCE!==undefined) tinyMCE.triggerSave();
+				if((beforeSubmit && beforeSubmit()===false) || (form.data('ht5ifv')!==undefined && !form.ht5ifv('valid'))){
+					form.stop().fadeTo(0,1)
+					return false;
+				}
+				var ajaxOptions={
+					type:'post',cache:false,
+					beforeSend:function(){submit.hide();submit.parent().append(imgLoadingSubmit);},
+					data:form.serialize(),
+					complete:function(){submit.show().blur();form.find('.img.imgLoading').remove();form.fadeTo(150,1)},
+					error:error
+				};
+				if(success) ajaxOptions.success=success;
+				$.ajax(url,ajaxOptions)
+					.success(function(){S.bodyIcon('tick',form);})
+					.error(function(){S.bodyIcon('tick',cross);});
+				return false;
+			})
+			/*.find(':submit').unbind('click').click(function(){
+				//var validator=form.data('validator');
+				//if(validator && !validator.checkValidity()) return false;
+				//submit=$(this);
+				return true;
+			});*/
+		});
 		return this;
 	};
 	
