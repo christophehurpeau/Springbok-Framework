bd19ecca9c0a4cd4ad27ad4c5bd24e8ff0130c20
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-09-19 15:41:30 +0200

    springbok.ajax : UPDATE_H1_WITH_TITLE

--- includes/js/springbok.ajax.js
+++ includes/js/springbok.ajax.js
@@ -21,7 +21,7 @@ includeCore('ui/slideTo');
 		S.ready=function(callback){ readyCallbacks.add(callback); };
 	});
 	S.redirect=function(url){ S.ajax.load(url); }
-	S.setTitle=function(title){ document.title=title; divVariable.children('h1:first').text(title) }
+	S.setTitle=function(title){ document.title=title; defineDefault('UPDATE_H1_WITH_TITLE',false); if(UPDATE_H1_WITH_TITLE) divVariable.children('h1:first').text(title) }
 	S.ajax={
 		init:function(){
 			$(document).on('click',
