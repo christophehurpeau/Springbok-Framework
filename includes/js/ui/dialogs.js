2d5b5f4a5b0920794d203325fcfc59eaf4ed2c65
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-12-03 18:23:34 +0100

    js : Starting springbok widget dialogs.

--- includes/js/ui/dialogs.js
+++ includes/js/ui/dialogs.js
@@ -1,6 +1,46 @@
+includeCore('ui/base');
+includeCore('libs/jquery-ui-1.9.2.position');
+S.WDialog=function(title,htmlOrText,buttons,options){
+	options=S.extendsObj({
+		open:true,
+		closeOnEscape:true,
+		zIndex:9000 //fancybox : 8030
+	},options);
+	this.elt=$('<div>')[S.isString(htmlOrText)?'text':'html'](htmlOrText)
+			.addClass('ui-dialog-content ui-widget-content');
+	
+};
+S.extendsClass(S.WDialog,S.Widget,{
+	dispose:function(){
+		this.uiDialog.remove();
+	},
+	_createWrapper:function(){
+		this.uiDialog = $( "<div>" )
+			.addClass( uiDialogClasses + this.options.dialogClass )
+			.hide()
+			.attr({
+				// setting tabIndex makes the div focusable
+				tabIndex: -1,
+				role: "dialog"
+			})
+		.appendTo('body');
+	}
+});
+
+
 S.dialogs={
+	newAlert:function(title,message){
+		var dialog,buttons={};
+		buttons[i18nc['Close']]=function(){dialog.close();};
+		dialog=new S.WDialog(title,message,buttons,{
+			position:{my:'center',at:'center'},
+			width:450,
+			modal:true
+		});
+	},
+	
 	alert:function(title,message){
-		var div=$('<div/>'),buttons={};
+		var div=$('<div>'),buttons={};
 		buttons[i18nc['Close']]=function(){$(this).dialog('close');};
 		S.isString(message) ? div.text(message) : div.html(message);
 		div.dialog({
@@ -15,7 +55,7 @@ S.dialogs={
 		});
 	},
 	confirm:function(title,message,okButtonName,callbackOk,callbackCancel){
-		var div=$('<div/>'),buttons={};
+		var div=$('<div>'),buttons={};
 		S.isString(message) ? div.text(message) : div.html(message);
 		
 		
@@ -43,13 +83,13 @@ S.dialogs={
 	},
 	
 	prompt:function(title,message,okButtonName,defaultVal,callback){
-		var div=$('<div/>'),buttons={};
+		var div=$('<div>'),buttons={};
 		if($.isFunction(defaultVal)){
 			callback=defaultVal;
 			defaultVal='';
 		}
 		S.isString(message) ? div.text(message) : div.html(message);
-		div.append($('<input type="text" class="wp100"/>').val(defaultVal).keydown(function(e){
+		div.append($('<input type="text" class="wp100">').val(defaultVal).keydown(function(e){
 			if(e.keyCode == '13'){
 				e.preventDefault();
 				e.stopImmediatePropagation();
@@ -79,7 +119,7 @@ S.dialogs={
 	},
 	
 	form:function(title,content,okButtonName,callback){
-		var div=$('<div/>'),buttons={};
+		var div=$('<div>'),buttons={};
 		div.html(content);
 		
 		buttons[i18nc.Cancel]=function(){div.dialog('close');};
