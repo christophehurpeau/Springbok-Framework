e2a46b4712ca86ddd9eaf0741b5888cf3c255670
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-12-26 12:20:11 +0100

    BTree field level_depth

--- behaviors/BTree_build.php
+++ behaviors/BTree_build.php
@@ -1,14 +1,15 @@
 <?php
 class BTree_build{
 	public static function onBuild($modelFile,&$contentInfos,$annotations,$enhanceConfig,&$classBeforeContent){
-		foreach(array('left','right','parent_id') as $fieldName)
+		foreach(array('left','right','parent_id','level_depth') as $fieldName)
 			if(isset($modelFile->_fields[$fieldName])) throw new Exception($modelFile->_className.' already contains a field "'.$fieldName.'"');
 		if(!isset($modelFile->_fields['id']))
 			throw new Exception($modelFile->_className.' must have an unique Pk "id"');
 		
-		$modelFile->_fields['left']=array('SqlType'=>array($modelFile->_fields['id']['SqlType']),'NotNull'=>false, 'NotBindable'=>false, 'Index'=>false);
-		$modelFile->_fields['right']=array('SqlType'=>array($modelFile->_fields['id']['SqlType']),'NotNull'=>false, 'NotBindable'=>false, 'Index'=>false);
-		$modelFile->_fields['parent_id']=array('SqlType'=>array($modelFile->_fields['id']['SqlType']),'NotNull'=>false, 'NotBindable'=>false,
+		$modelFile->_fields['parent_id']=array('SqlType'=>$modelFile->_fields['id']['SqlType'],'Null'=>false, 'NotBindable'=>false,
 				'ForeignKey'=>array($modelFile->_className,'id','onDelete'=>'RESTRICT'));
+		$modelFile->_fields['left']=array('SqlType'=>$modelFile->_fields['id']['SqlType'],'NotNull'=>false, 'NotBindable'=>false, 'Index'=>false);
+		$modelFile->_fields['right']=array('SqlType'=>$modelFile->_fields['id']['SqlType'],'NotNull'=>false, 'NotBindable'=>false, 'Index'=>false);
+		$modelFile->_fields['level_depth']=array('SqlType'=>array('tinyint(3) unsigned'),'NotNull'=>false, 'NotBindable'=>false, 'Index'=>false);
 	}
 }
