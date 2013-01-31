cbb158b9b8083267141b7a84efbe3125116fdadd
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2013-01-31 11:02:30 +0100

    UFile::readLine()

--- utils/UFile.php
+++ utils/UFile.php
@@ -78,6 +78,9 @@ class UFileOpened{
 		//return null;
 		return stream_get_contents($this->_file);
 	}
+	public function readLine(){
+		return stream_get_line($this->_file,16384);
+	}
 	
 	public function lockShared(){
 		return flock($this->_file,LOCK_SH);
