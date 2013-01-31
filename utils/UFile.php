f2e2af38717ff394d1af6a6d462ae236ab40fb0f
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2013-01-31 11:06:33 +0100

    UFile::readLine() : add length

--- utils/UFile.php
+++ utils/UFile.php
@@ -78,8 +78,8 @@ class UFileOpened{
 		//return null;
 		return stream_get_contents($this->_file);
 	}
-	public function readLine(){
-		return stream_get_line($this->_file,16384);
+	public function readLine($length=8192){
+		return stream_get_line($this->_file,$length);
 	}
 	
 	public function lockShared(){
