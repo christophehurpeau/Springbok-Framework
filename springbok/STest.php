35b7157b0b15fecb9154b7595338baa56d73138a
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2013-02-05 10:55:01 +0100

    STest : checkHeadLinks()

--- springbok/STest.php
+++ springbok/STest.php
@@ -1,7 +1,10 @@
 <?php
 class TestNavigator extends CHttpClient{
 	public function get($url,$entry='index'){
-		return parent::get($this->_url($url,$entry));
+		try{ return parent::get($this->_url($url,$entry)); }catch(HttpClientError $e){}
+	}
+	public function getReal($url,$entry='index'){
+		return $this->get($url[0]==='/' ? '\\'.$url : $url,$entry);
 	}
 	public function post($url,$entry='index'){
 		return parent::post($this->_url($url,$entry));
@@ -21,9 +24,28 @@ class TestNavigator extends CHttpClient{
 		assert($this->getStatus()===200);
 	}
 	
+	protected function _beforeCurlCreate(){
+		$this->parsedHtml=null;
+	}
+	
+	private $parsedHtml;
 	public function parseHtml(){
 		include_once CLIBS.'simple_html_dom.php';
-		return str_get_html($this->getResult());
+		return $this->parsedHtml=str_get_html($this->getResult());
+	}
+	private function _parseHtml(){
+		if($this->parsedHtml===null) return $this->parseHtml();
+		return $this->parsedHtml;
+	}
+	
+	public function checkHeadLinks(){
+		$parsedHtml=$this->_parseHtml();
+		$links=$parsedHtml->find('head link');
+		foreach($links as $link){
+			$this->getReal($link->href);
+			$this->assertStatus200();
+		}
+		$this->parsedHtml=$parsedHtml;
 	}
 }
 
