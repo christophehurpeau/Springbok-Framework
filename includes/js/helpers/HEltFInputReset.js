b66193c4f744884160c747a7a568444ca56cf6e6
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2013-02-14 14:52:22 +0100

    S.HEltFInputReset.superCtor

--- includes/js/helpers/HEltFInputReset.js
+++ includes/js/helpers/HEltFInputReset.js
@@ -1,6 +1,6 @@
 S.HEltFInputReset=S.extClass(S.HEltFContble,{
 	ctor:function(form,title){
-		S.HEltFInputReset.superctor.call(this,form);
+		S.HEltFInputReset.superCtor.call(this,form);
 		this.elt=$('<input type="reset" class="submit reset">')/*.data('sElt',this)*/.attr('value',title===undefined?i18nc.Save:title);
 	},
 	container:function(){ return new S.HEltFCont(this,'submit reset'); },
