14d25c3b2c1959d04ab4642f6344f0e588f01e6d
Christophe Hurpeau <christophe@hurpeau.com>
2013-03-22 17:10:55 +0100

    OpenGraph metas

--- helpers/HOpenGraphMeta.php
+++ helpers/HOpenGraphMeta.php
@@ -9,10 +9,18 @@ class HOpenGraphMeta{
 		HHead::metaProperty('og:title',$title);
 		/* DEV */ return '<div style="color:red;font-size:12pt">Please do not echo HOpenGraphMeta::title()</div>'; /* /DEV */
 	}
+	public static function description($description){
+		HHead::metaProperty('og:description',$description);
+		/* DEV */ return '<div style="color:red;font-size:12pt">Please do not echo HOpenGraphMeta::description()</div>'; /* /DEV */
+	}
 	
 	/** That image should be at least 50x50 in any of the usually supported image forms (JPG, PNG, etc.) */
-	public static function image($url){
+	public static function image($url,$type=null,$width=null,$height=null){
 		HHead::metaProperty('og:image',$url);
+		if($type!==null) HHead::metaProperty('og:image:type',$type);
+		if($width!==null) HHead::metaProperty('og:image:width',$width);
+		if($height!==null) HHead::metaProperty('og:image:height',$height);
+		
 		/* DEV */ return '<div style="color:red;font-size:12pt">Please do not echo HOpenGraphMeta::image()</div>'; /* /DEV */
 	}
 	
@@ -25,4 +33,15 @@ class HOpenGraphMeta{
 		HHead::metaProperty('fb:app_id',$appId);
 		/* DEV */ return '<div style="color:red;font-size:12pt">Please do not echo HOpenGraphMeta::fbApp()</div>'; /* /DEV */
 	}
+	
+	public static function locale($locale){
+		HHead::metaProperty('og:locale',$locale);
+		/* DEV */ return '<div style="color:red;font-size:12pt">Please do not echo HOpenGraphMeta::locale()</div>'; /* /DEV */
+	}
+	
+	public static function type($type){
+		HHead::metaProperty('og:type',$type);
+		/* DEV */ return '<div style="color:red;font-size:12pt">Please do not echo HOpenGraphMeta::type()</div>'; /* /DEV */
+	}
+	
 }
