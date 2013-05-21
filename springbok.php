4e9dfd05f90590db8a0ccac99c17c7fa3cb35e62
Christophe Hurpeau <christophe@hurpeau.com>
2013-05-21 11:36:08 +0200

    CLang default lang when errors

--- springbok.php
+++ springbok.php
@@ -162,7 +162,7 @@ class Springbok{
 function __autoload($className){ /* DEV */
 	if($className==='Config'){
 		eval('class Config{public static $autoload_default,$cookie_domain=array("'.Springbok::$scriptname.'"=>""),$models_infos,'
-			.'$default_lang="'.($lang=(file_exists(dirname(APP).'src/locales/fr.yml')?'fr':'en')).'",$availableLangs=array("'.$lang.'"),$siteUrl=array("index"=>""),'
+			.'$allLangs=array("'.($lang=(file_exists(dirname(APP).'src/locales/fr.yml')?'fr':'en')).'"),$availableLangs=array("'.$lang.'"),$siteUrl=array("index"=>""),'
 			.'$db=array("default"=>array("dbname"=>"mysql","user"=>"mysql","password"=>"mysql"));}'
 			.'Config::$autoload_default=APP.\'models/\';Config::$models_infos=Config::$autoload_default."infos/";');
 		define("STATIC_URL",BASE_URL.'/web/');
