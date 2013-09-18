54d8a1edd4acfaacdda96f4acc633e2ce3373ba8
Christophe Hurpeau <christophe@hurpeau.com>
2013-09-18 19:07:13 +0200

    comments

--- utils/UEncoding.php
+++ utils/UEncoding.php
@@ -1,21 +1,44 @@
 <?php
+/** Utils for String encodings */
 class UEncoding{
+	/**
+	 * Try to detect the encoding of the string a try to convert it to UTF-8
+	 * 
+	 * @param string
+	 * @return string
+	 */
 	public static function convertToUtf8($str){
 		if(($enc=mb_detect_encoding($str,'UTF-8, ISO-8859-15, ASCII, GBK'))!=='UTF-8')
 			$str=iconv($enc,'UTF-8',$str); 
 		return $str;
 	}
 	
+	/**
+	 * Try to detect if the string is UTF-8
+	 * 
+	 * @param string
+	 * @return bool
+	 */
 	public static function isUtf8($str){
 		return mb_detect_encoding($str,'UTF-8');
 	}
 	
-	/** ISO-8859-1 to UTF-8 */
+	/**
+	 * ISO-8859-1 to UTF-8
+	 * 
+	 * @param string
+	 * @return string
+	 */
 	public static function toUtf8($str){
 		return iconv('ISO-8859-1','UTF-8//TRANSLIT',$str);
 	}
 	
-	/** UTF-8 to ISO-8859-1 */
+	/**
+	 * UTF-8 to ISO-8859-1
+	 * 
+	 * @param string
+	 * @return string
+	 */
 	public static function fromUtf8($str){
 		return iconv('UTF-8','ISO-8859-1//TRANSLIT',$str);
 	}
--- utils/UExec.php
+++ utils/UExec.php
@@ -1,6 +1,14 @@
 <?php
 /** http://www.cyberciti.biz/faq/linux-unix-reuse-openssh-connection/ */
+/** Utils for executing commands in the shell */
 class UExec{
+	/**
+	 * Create a persistent connexion for a limited time (using a sleep)
+	 * 
+	 * @param array ssh options
+	 * @param int time in seconds to keep it opened
+	 * @return mixed
+	 */
 	public static function createPersistantSsh($ssh,$for=120){
 			$sshCommand=self::_sshCommand($ssh).'-f ';
 			if(!empty($ssh['user'])) $sshCommand.=$ssh['user'].'@';
@@ -9,6 +17,15 @@ class UExec{
 			return self::execWithSshAgent($sshCommand.'"sleep '.((int)$for).'"',$ssh);
 	}
 	
+	/**
+	 * Do a rsync
+	 * 
+	 * @param string source
+	 * @param string destination
+	 * @param array options ['simulation'=>true|false, 'exclude'=>array, 'ssh'=>options]
+	 * @param int time in seconds to keep it opened
+	 * @return mixed
+	 */
 	public static function rsync($source,$dest,$options,$rsyncOptions=false){
 		/* bug : rsync: getcwd(): No such file or directory (2) */
 		$command='cd / && rsync -'.($rsyncOptions!==false ? $rsyncOptions : ($options['simulation'] === true ? 'rtvnzC' : 'rtvlzC').' --delete');
@@ -38,17 +55,31 @@ class UExec{
 		return self::exec($command);
 	}
 	
+	/**
+	 * Copy a file using cp or scp if ssh
+	 * 
+	 * @param string
+	 * @param string
+	 * @param bool|array
+	 * @return mixed
+	 */
 	public static function copyFile($source,$dest,$ssh=false){
 		if($ssh) return self::execWithSshAgent($cmd='scp '.escapeshellarg($source).' '.escapeshellarg((empty($ssh['user'])?'':$ssh['user'].'@').$ssh['host'].':'.$dest),$ssh);
 		else return self::exec('cp '.escapeshellarg($source).' '.escapeshellarg($dest));
 	}
 	
-	public static function php($args,$ssh=false){
+	/**
+	 * Execute a php file
+	 * 
+	 * @param string $args...
+	 * @return mixed
+	 */
+	public static function php($args){
 		$args=func_get_args();
 		$cmd='php';
 		foreach($args as $key=>&$arg)
 			$cmd.=' '.escapeshellarg($arg);
-		return self::exec($cmd,$ssh=false);
+		return self::exec($cmd,false);
 	}
 
 	private static function _sshCommand($ssh){
@@ -82,6 +113,14 @@ class UExec{
 		return $sshCommand;
 	}
 	
+	/**
+	 * Execute a command in the shell and return the result in UTF-8
+	 * 
+	 * @param string
+	 * @param bool|array
+	 * @param bool
+	 * @return string
+	 */
 	public static function exec($command,$ssh=false,$waiting=true){
 		if($ssh){
 			$sshCommand=self::_sshCommand($ssh);
@@ -94,6 +133,15 @@ class UExec{
 		return UEncoding::convertToUtf8(trim(shell_exec($command.($waiting?' 2>&1':' > /dev/null 2>/dev/null &'))));
 	}
 	
+	/**
+	 * Create a .tar.gz archive
+	 * 
+	 * @param the directory to go to
+	 * @param string options '' or start with ' ', escape params yourself
+	 * @param string name of the archive
+	 * @param array a list of files
+	 * @return string the result of the command
+	 */
 	public static function createTarGz(/*#if false*/$cd,$options,$archive,$files/*#/if*/){
 		$files=func_get_args();
 		$cd=array_shift($files);
@@ -102,6 +150,12 @@ class UExec{
 		return self::exec('cd '.escapeshellarg($cd).' && tar'.$options.' -czf '.escapeshellarg($archive).' '.implode(' ',array_map('escapeshellarg',$files)));
 	}
 	
+	/**
+	 * Escape a path, to use with rm
+	 * 
+	 * @param string
+	 * @return string the escaped path
+	 */
 	public static function rmEscape($path){
 		return preg_replace('/([\*\[\]\?\!])/','\\\$1',$path);
 	}
