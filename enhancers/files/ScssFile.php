4d220c9eb58afb85e660fbfbfadc020b170fa53b
Christophe Hurpeau <christophe@hurpeau.com>
2013-10-02 11:43:59 +0200

    compass install

--- enhancers/files/ScssFile.php
+++ enhancers/files/ScssFile.php
@@ -1,12 +1,12 @@
 <?php
 class ScssFile extends EnhancerFile{
 	public static $CACHE_PATH='scss_8.1';
-	
+
 	public static function init(){
 		self::$preprocessor=new Preprocessor('scss');
 		ScssFile::findSassPath();
 	}
-	
+
 	public function loadContent($srcContent){
 		if(!$this->isCore()){
 			if(file_exists($filename=dirname($this->srcFile()->getPath()).'/_mixins.scss'))
@@ -14,13 +14,13 @@ class ScssFile extends EnhancerFile{
 			$srcContent=file_get_contents(CORE_SRC.'includes/scss/mixins.scss').
 						file_get_contents(CORE_SRC.'includes/scss/functions.scss').$srcContent;
 		}
-		
+
 		$currentPath=dirname($this->srcFile()->getPath());
 		$includes=array('colors'=>false);
 		$this->_srcContent=self::includes($srcContent,$currentPath,$includes,$this->enhanced,substr($this->srcFile()->getName(),0,-5));
 	}
-	
-	
+
+
 	public static function &includes($content,$currentPath,&$includes,&$enhanced,$scssFilename){
 		$content=preg_replace_callback('/@include(Core|Lib|Plugin)?\s+\'([\w\s\._\-\/]+)\'\;/Ui',function($matches)
 																				use($currentPath,&$includes,&$enhanced,$scssFilename){
@@ -29,7 +29,7 @@ class ScssFile extends EnhancerFile{
 			if($matches[2]==='base/buttonsOverride.scss') $matches[2]='base/buttons.scss';
 			elseif(isset($includes[$matches[1]][$matches[2]])) return '';
 			$includes[$matches[1]][$matches[2]]=1;
-			
+
 			$nextContent='';
 			/*if(!empty($matches[1]) && $matches[1]==='Core') */;
 			if(empty($matches[1])){
@@ -49,19 +49,19 @@ class ScssFile extends EnhancerFile{
 				}else{
 					$filename=$matches[1]==='Lib' ? dirname(CORE_SRC).'/' : CORE_SRC;
 					$filename.='includes/';
-					
+
 					$folderName=$isStylus ? 'styl' : ($matches[1]==='Lib'?'css/':'scss/');
 					if(file_exists($filename.$folderName.$matches[2])) $filename.=$folderName;
 				}
 			}
 			$filename.=$matches[2];
-			
+
 			return ScssFile::includes(file_get_contents($filename).$nextContent,$currentPath,$includes,$enhanced,$scssFilename);
 		},$content);
 		return $content;
 	}
-	
-	
+
+
 	public function enhanceContent(){
 		$this->_srcContent=$this->preprocessor($this->_srcContent);
 		$rules=array(
@@ -89,7 +89,7 @@ class ScssFile extends EnhancerFile{
 
 	public function getEnhancedDevContent(){}
 	public function getEnhancedProdContent(){}
-	
+
 	public function writeDevFile($devFile){
 		$this->callSass($this->_srcContent,$devFile->getPath(),true);
 		if(($appDir=$this->enhanced->getAppDir()) && !$this->isCore()){
@@ -108,14 +108,14 @@ class ScssFile extends EnhancerFile{
 		return true;
 	}
 
-	
+
 	protected function copyFromCache($cachefile,$devFile,$prodFile,$justDev){
 		parent::copyFromCache($cachefile,$devFile,$prodFile,$justDev);
 		$tmpDir=$this->enhanced->getTmpDir();
 		if(!file_exists($tmpDir.'compiledcss/')) mkdir($tmpDir.'compiledcss/',0755,true);
 		$devFile->copyTo($tmpDir.'compiledcss/'.$devFile->getName());
 	}
-	
+
 	private static $sassExecutable='sass';
 	public static function findSassPath(){
 		if(file_exists('/var/lib/gems/1.8/bin/sass')) self::$sassExecutable='/var/lib/gems/1.8/bin/sass';
@@ -123,7 +123,7 @@ class ScssFile extends EnhancerFile{
 		if(stripos($sassVersion,'error')) throw new Exception('Error in sass : '.$sassVersion);
 		if(!preg_match('/(^|\s+)([0-9]+\.[0-9]+)/',$sassVersion,$sassMatchVersion) || empty($sassMatchVersion[2])) throw new Exception('Unable to find version : '.$sassVersion);
 		$sassVersion = (float)$sassMatchVersion[2];
-		if($sassVersion < 3.2) throw new Exception('Please update your sass version : sudo gem install sass ('.$sassVersion.')');
+		if($sassVersion < 3.2) throw new Exception('Please update your sass version : sudo gem install sass compass ('.$sassVersion.')');
 	}
 	public function callSass($content,$destination){
 		$dest=$destination?$destination:tempnam($this->enhanced->getTmpDir(),'scssdest');
@@ -141,14 +141,14 @@ class ScssFile extends EnhancerFile{
 		$content=preg_replace_callback('/filter\:progid\:DXImageTransform\.Microsoft\.gradient\(startColorstr=(?:\'|\")#([0-9A-F]{3,6})(?:\'|\"),endColorstr=(?:\'|\")#([0-9A-F]{3,6})(?:\'|\")([^)]*)/',
 				function($m){return "filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#".(strlen($m[1])===3?$m[1].$m[1]:$m[1])."',endColorstr='#".(strlen($m[2])===3?$m[2].$m[2]:$m[2])."'".$m[3]; },$content);
 		CssFile::executeCompressor($this->enhanced->getTmpDir(),$content,$dest);
-		
+
 		if(!$destination){
 			$destination=file_get_contents($dest);
 			unlink($dest);
 			return $destination;
 		}
 	}
-	
+
 	public static function afterEnhanceApp(&$enhanced,&$dev,&$prod){
 		CssFile::afterEnhanceApp($enhanced,$dev,$prod);
 	}
