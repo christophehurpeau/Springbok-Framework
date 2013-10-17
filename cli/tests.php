7a605d51548fe66d963b659a31ef07a47d6a9680
Christophe Hurpeau <christophe@hurpeau.com>
2013-10-17 16:42:45 +0200

    Update tests.php

--- cli/tests.php
+++ cli/tests.php
@@ -1,6 +1,7 @@
 <?php
 
 if(!isset($folderTests)) $folderTests=APP.'tests/';
+if(!file_exists($folderTests)){ echo 'No tests'; exit(0); }
 $t=microtime(true);
 $tests=new RecursiveDirectoryIterator($folderTests,FilesystemIterator::SKIP_DOTS); $l=strlen($folderTests); $total=$totalFailed=0;
 UPhp::recursive(function($callback,$tests) use($l,&$total,&$totalFailed){
@@ -19,6 +20,6 @@ UPhp::recursive(function($callback,$tests) use($l,&$total,&$totalFailed){
 
 $t=microtime(true) - $t;
 echo "\n";
-if($total===0){ echo 'No tests'; exit(2); }
+if($total===0){ echo 'No tests'; exit(0); }
 else if($totalFailed===0){ echo cliColor('OK',CliColors::green).' '.$total.'/'.$total.' in '.$t.' ms'; exit(0); }
 else{ echo cliColor('FAILED',CliColors::lightRed).' '.($total-$totalFailed).'/'.$total.' in '.$t.' ms'; exit(1); }
