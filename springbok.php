0509fafa5b5e1b7089231627db9e3e9f7ce83c81
Christophe Hurpeau <christophe@hurpeau.com>
2013-11-08 15:01:10 +0100

    Update springbok.php
    Springbok::getErrorText E_USER_NOTICE

--- springbok.php
+++ springbok.php
@@ -168,17 +168,17 @@ class Springbok{
 			case E_WARNING: return 'WARNING';
 			case E_PARSE: return 'PARSE';
 			case E_NOTICE: return 'NOTICE';
+			case E_DEPRECATED: return 'DEPRECATED';
+			case E_STRICT: return 'STRICT';
 			case E_CORE_ERROR: return 'CORE_ERROR';
 			case E_CORE_WARNING: return 'CORE_WARNING';
 			case E_COMPILE_ERROR: return 'COMPILE_ERROR';
 			case E_COMPILE_WARNING: return 'COMPILE_WARNING';
 			case E_USER_ERROR: return 'USER_ERROR';
 			case E_USER_WARNING: return 'USER_WARNING';
-			case E_NOTICE: return 'USER_NOTICE';
-			case E_STRICT: return 'STRICT';
-			case E_RECOVERABLE_ERROR: return 'RECOVERABLE_ERROR';
-			case E_DEPRECATED: return 'DEPRECATED';
+			case E_USER_NOTICE: return 'USER_NOTICE';
 			case E_USER_DEPRECATED: return 'USER_DEPRECATED';
+			case E_RECOVERABLE_ERROR: return 'RECOVERABLE_ERROR';
 		}
 		return 'Unknown';
 	}
